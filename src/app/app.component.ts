6f3f093e5d42ca6896246f1e8e10a165b5501019
Jelena Basaric <jbasaric83@gmail.com>
2023-05-10 10:07:12 +0200

    method isEmptyString for button disabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   username=''
 
-  isEmptyString(event: any){
-    console.log(event);
+  isEmptyString(){
+   return this.username===""? true:false
   }
   onDelete(){
     this.username=""
